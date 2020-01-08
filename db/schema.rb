b5f0d1e3aa14b147a6f492f734e8e694614b4891
MarioDena <enrique.dn@outlook.com>
2020-01-08 13:12:57 -0600

    [Fix] Added redirection to user controller when no user is logged in

--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(version: 2020_01_08_164204) do
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "events", "users", column: "creator_id"
