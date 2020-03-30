b36a131860b59f662b4f6b37cd755eea93045e9d
mike <michgoldennathan17@gmail.com>
2020-03-30 03:05:51 +0100

    [Feature] Change auth feature, change style on events index

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,29 @@
 
 class SessionsController < ApplicationController
   skip_before_action :auth?, only: %i[new create]
+  before_action :signed_in?
 
   def new; end
 
   def create
     user = User.find_by(email: params[:session][:email])
     if user
+      # &.authenticate(params[:session][:password])
+      redirect_back fallback_location: root_path
       remember(user)
-      redirect_to root_path
       flash[:success] = 'Signed in successfully'
     else
-      flash.now[:danger] = 'User doesn\'t exist'
+      flash.now[:danger] = 'Account doesn\'t exist'
       render 'new'
     end
+  end
+
+  private
+
+  def signed_in?
+    return unless current_user
+
+    flash[:alert] = 'You are signed in.'
+    redirect_to root_path
   end
 end
